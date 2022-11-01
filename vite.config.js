8fc8462baf488ee2b1ace4b8f6ad01fdd40c8c2f
Lekkice <lekkice0@gmail.com>
2022-10-31 23:36:45 -0300

    fixed path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "https://rekkice.github.io/college/" : "/",
+  base: process.env.NODE_ENV === "production" ? "https://rekkice.github.io/collage/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
