f51f81f8f75f21a38bc1a87198353bf1d3d9e347
Lekkice <lekkice0@gmail.com>
2022-11-01 00:02:46 -0300

    fixed path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "./" : "/",
+  base: process.env.NODE_ENV === "production" ? "/collage/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
